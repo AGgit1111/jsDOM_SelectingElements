bb7167b4383a72d4b3228f6359df2455ea5ae6fc
AGgit <ag.git1111@gmail.com>
2023-10-23 12:16:02 -0600

    Created basic HTML, CSS, and JS files, selected elements and logged to console

---
+++ domManipulation.js
@@ -0,0 +1,20 @@
+// Go Live, inspector, then Console- to see selected elements logged.
+
+// Selecting elements by ID:
+const mainTitle = document.getElementById('main-title');
+console.log(mainTitle);
+
+const subtitle = document.getElementById('subtitle');
+console.log(subtitle);
+
+const paragraphTitle = document.getElementById('paragraph-title');
+console.log(paragraphTitle);
+
+// Selecting elements by class:
+const paragraph = document.querySelector('.paragraph');
+console.log(paragraph);
+
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => {
+    console.log(box);
+});
